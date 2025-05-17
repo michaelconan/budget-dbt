5e8f373f01e1180954b45f56d8fb2ec77e1bbaa5
Michael Conan <michaelbconan@gmail.com>
2025-05-17 01:26:10 +0100

    feat: add transfer macro, regex test, transfer logic
    - add macro to identify inter-account transactions by date and amount
    - add custom regex test to validate date format
    - add transfer logic and transformations to bofa and final entity model

--- dbt/models/staging/revolut/stg_revolut__transactions.sql
+++ dbt/models/staging/revolut/stg_revolut__transactions.sql
@@ -5,8 +5,8 @@ with personal as (
     select
         "type",
         product,
-        substr(started_date, 1, 10) as started_date,
-        substr(completed_date, 1, 10) as completed_date,
+        started_date,
+        completed_date,
         "description",
         amount,
         fee,
@@ -23,8 +23,8 @@ joint as (
     select
         "type",
         'Joint' as product,
-        substr(started_date, 1, 10) as started_date,
-        substr(completed_date, 1, 10) as completed_date,
+        started_date,
+        completed_date,
         "description",
         amount,
         fee,
@@ -51,41 +51,50 @@ combined as (
 -- 3. Create a surrogate key for each transaction
 
 keyed as (
+
     select
-                    {{ dbt_utils.generate_surrogate_key(
-                            ['product', 'completed_date', 'description', 'amount', 'balance']
-                        )
-                    }}
+        {{ dbt_utils.generate_surrogate_key(
+                ['product', 'completed_date', 'description', 'amount', 'balance']
+            )
+        }}
             as transaction_key,
         *
     from
         combined
+
 ),
 
 -- 4. Identify transfers between accounts
 
 transfers as (
 
-    select t1.transaction_key
+    {{ get_transfer_transactions('keyed', 'completed_date', 'product', 'amount') }}
+
+),
+
+-- 5. Select all transactions and indicate if they are transfers
+flagged as (
+
+    select
+        cast(k.transaction_key as text) as transaction_key,
+        "type",
+        cast(product as text) as product,
+        cast(substr(started_date, 1, 10) as text) as started_date,
+        cast(substr(completed_date, 1, 10) as text) as completed_date,
+        "description",
+        amount,
+        fee,
+        currency,
+        "state",
+        balance,
+        (t.transaction_key is not null) as is_transfer
     from
-        keyed as t1
-    inner join
-        keyed as t2
-        on
-            t1.completed_date = t2.completed_date
-            and t1.amount = -1 * t2.amount
-            and t1.product != t2.product
-            and t1.type = t2.type
+        keyed as k
+    left join
+        transfers as t
+        on k.transaction_key = t.transaction_key
 
 )
 
--- 5. Select all transactions and indicate if they are transfers
-
-select
-    k.*,
-    t.transaction_key is not null as is_transfer
-from
-    keyed as k
-left join
-    transfers as t
-    on k.transaction_key = t.transaction_key
+select *
+from flagged
